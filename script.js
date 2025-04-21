b5b57ccb6ca84ce83f8b39f855d597399435dfdf
haiderqal <haideralime@icloud.com>
2025-04-20 23:58:04 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("Hey Haider");
+
+let nam ="Adam";
+
+let surnam = "Doe";
+
+console.log(nam);
+console.log(surnam);
+
+
+let age = 11;
+console.log(age);
+
+age = 54;
+
+console.log(age);
